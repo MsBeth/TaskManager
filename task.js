b8d8cc9df598115aef3bd0314ef1cbbba4d4e651
Maribeth Calipo <maribethsc43@gmail.com>
2021-10-19 12:12:44 -0400

    Added feature displaying all the task to the user.

--- task.js
+++ task.js
@@ -9,12 +9,42 @@ What would you like to do (Please enter one of the option below):
 "REMOVE" - Remove A Task
 "CLOSE" - Close The Task Manager
 `;
+
+// Array for storing tasks
+const tasks = [
+  `Charge MacBook`,
+  `Master JavaScript`
+];
+
+//For displaying task to the user
+let showTasks = ``;
+
+
 // Display the menu for the user to seleect an option.
 
 let userInput = prompt(menu);
 
 //Loops and continues to display the menu until the user ends/closes the Task Manager(AKA until the user enters CLOSE)
 while (userInput !== `CLOSE`){
+
+    //Check if user entered TASKS
+    if (userInput === `TASK`){
+
+      //The For Of Loop is used here to concatenate each task (AKA Item)in the task array to the ShowTasks string Variable
+      for (task of tasks){
+        
+        //Using \n to create a new line (AKA Line break)after each task
+        showTasks += `${task}\n`;
+     }
+
+    //Displace the current tasks to the user
+    alert(showTasks);
+
+    //Set the value of the showTasks string varieble back to an empty string
+    showTasks = "";
+
+    }
+    
 
     //Display the menu again
     userInput = prompt (menu);
