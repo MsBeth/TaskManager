a77ec0086f0f6b013cc7a3ec1fcba7f24ce9f704
Maribeth Calipo <maribethsc43@gmail.com>
2021-10-19 11:02:14 -0400

    Created the task.js file. Also, set up menu.

---
+++ task.js
@@ -0,0 +1,14 @@
+// TASK MANAGER
+
+// Using Template Literal bec it recognozed a line breaks
+const menu =`TASK MANAGER
+
+What would you like to do (Please enter one of the option below):
+"TASK" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager
+`;
+// Display the menu for the user to seleect an option.
+
+let userInput = prompt(menu);
